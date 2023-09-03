4b8794706047490d2e4ac22240d42cac85fd8225
ratnakottara <140639468+ratnakottara@users.noreply.github.com>
2023-09-03 19:28:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+# Taking input from the user
+user_input = input("Please enter something: ")
+
+# Generating output based on the input
+output = "You entered: " + user_input
+
+# Printing the output
+print(output)
